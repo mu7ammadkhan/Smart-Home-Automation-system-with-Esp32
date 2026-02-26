064413aa967fd466abc6841117c3bb86066fac9f
Mu7ammad Khan <117804096+mu7ammadkhan@users.noreply.github.com>
2026-02-26 23:51:14 +0500

    added read tokens from Db

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,79 @@
+import { NextResponse } from "next/server";
+import Jwt, { JwtPayload } from "jsonwebtoken";
+
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/Users";
+
+type TokenPayload = JwtPayload & {
+  userID?: string;
+};
+
+export async function GET(req: Request) {
+  try {
+    // Read token from authorization header : "Bearear <token>"
+    const authHeader = req.headers.get("authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized: Token missing" },
+        { status: 401 },
+      );
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized: missing tokens" },
+        { status: 401 },
+      );
+    }
+
+    // Token verification
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Server missconfigured: JWT_SECRET missing",
+        },
+        { status: 500 },
+      );
+    }
+
+    let decoded: TokenPayload;
+    try {
+      decoded = Jwt.verify(token, secret) as TokenPayload;
+    } catch {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized: invalid token" },
+        { status: 401 },
+      );
+    }
+
+    const userID = decoded.userID || (decoded as any).uid;
+    if (!userID) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized: invalid token payload" },
+        { status: 401 },
+      );
+    }
+
+    // Fetch user
+    await connectDB();
+    const user = await User.findById(userID).select(
+      "_id name email createdAt ",
+    );
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized: user not found" },
+        { status: 401 },
+      );
+    }
+
+    return NextResponse.json({ success: true, user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Server error" },
+      { status: 500 },
+    );
+  }
+}
